1b7979e6863424b622cbee6095b61bef572f4bcc
Thayssa Bernardo <thayssabernardo@gmail.com>
2023-01-26 20:07:22 -0300

    Criando classes Pessoa e Funcionario, com seus getters e setters

--- src/main/java/desafioTecnico/model/Pessoa.java
+++ src/main/java/desafioTecnico/model/Pessoa.java
@@ -1,2 +1,30 @@
-package desafioTecnico.model;public class Pessoa {
+package desafioTecnico.model;
+
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+
+    protected Long id;
+    protected String nome;
+    protected LocalDate nascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
 }
