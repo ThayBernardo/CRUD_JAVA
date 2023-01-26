218907485d97d355b318040f641d15641c8d0eac
Thayssa Bernardo <thayssabernardo@gmail.com>
2023-01-26 20:09:16 -0300

    Criando classe Principal e utilizando metodo create nela

--- src/main/java/desafioTecnico/Principal.java
+++ src/main/java/desafioTecnico/Principal.java
@@ -1,4 +1,23 @@
 package desafioTecnico;
 
-public class Application {
+import desafioTecnico.dao.FuncionariosDAO;
+import desafioTecnico.model.Funcionarios;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        FuncionariosDAO funcionariosDAO = new FuncionariosDAO();
+
+        Funcionarios funcionarios = new Funcionarios();
+        funcionarios.setNome("Maria");
+        funcionarios.setNascimento(LocalDate.of(2000, 10, 18));
+        funcionarios.setSalario(BigDecimal.valueOf(2009.44));
+        funcionarios.setFuncao("Operador");
+
+        funcionariosDAO.create(funcionarios);
+    }
 }
